ec69be45dcf16fdb5136d6889c5ca2fbd2898a67
Slavent <startupne@gmail.com>
2018-11-02 03:43:01 +0700

    karma

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -1,24 +1,25 @@
-import './home.css';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { Breadcrumb, BreadcrumbItem, Alert, Col, Row } from 'reactstrap';
-import { getSession } from 'app/shared/reducers/authentication';
-import { getEntities as getEmployees } from 'app/entities/employee/employee.reducer';
-import PerformerHome from 'app/modules/home/performerHome';
-import { EmployeeRole } from 'app/shared/model/employee.model';
+import "./home.css";
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { Breadcrumb, BreadcrumbItem, Alert, Col, Row } from "reactstrap";
+import { getSession } from "app/shared/reducers/authentication";
+import { getEntities as getEmployees } from "app/entities/employee/employee.reducer";
+import PerformerHome from "app/modules/home/performerHome";
+import { EmployeeRole } from "app/shared/model/employee.model";
 
-export interface IHomeProp extends StateProps, DispatchProps {}
+export interface IHomeProp extends StateProps, DispatchProps {
+}
 
 export class Home extends React.Component<IHomeProp> {
-  componentDidMount() {
-    this.props.getSession();
-    this.props.getEmployees();
+  componentDidMount () {
+    this.props.getSession ();
+    this.props.getEmployees ();
   }
 
-  render() {
+  render () {
     const { account, employees, isAuthenticated } = this.props;
-    const employee = employees.find(item => item.username.toLocaleLowerCase() === account.login);
+    const employee = employees.find ( item => item.username.toLocaleLowerCase () === account.login );
 
     return (
       <Row>
@@ -26,37 +27,37 @@ export class Home extends React.Component<IHomeProp> {
           md="12"
           style={{
             background: !isAuthenticated
-              ? 'url(https://insights.dice.com/wp-content/uploads/2018/03/Hipster-Developer-Dice.jpg) center center'
-              : '',
+              ? "url(https://insights.dice.com/wp-content/uploads/2018/03/Hipster-Developer-Dice.jpg) center center"
+              : "",
             minHeight: 600
           }}
         >
           {isAuthenticated ? (
             <div>
-              {/*<Breadcrumb>*/}
-              {/*<BreadcrumbItem active>Главная</BreadcrumbItem>*/}
-              {/*</Breadcrumb>*/}
-              <h2>
-                Здравствуйте, {account.firstName} {account.lastName}!
-              </h2>
+              <Breadcrumb>
+                <BreadcrumbItem active>Главная</BreadcrumbItem>
+              </Breadcrumb>
+              <h1>Здравствуйте, {account.firstName ? account.firstName + " " + account.lastName : "новый пользователь"}!</h1>
+              <p className="lead">В данной системе вам доступен поиск исполнителей для ваших задач.</p>
               <ul>
-                {employee &&
-                  employee.role === EmployeeRole.CUSTOMER && (
-                    <li>
-                      <Link to={'/entity/task-creation'}>Оформить заявку</Link>
-                    </li>
-                  )}
-                {employee &&
-                  employee.role === EmployeeRole.CUSTOMER && (
-                    <li>
-                      <Link to={'/entity/customer-requests'}>Мои заявки</Link>
-                    </li>
-                  )}
-                {employee && employee.role === EmployeeRole.PERFORMER && <PerformerHome />}
+                <li>Заказчик публикует заявку на исполнение задачи.</li>
+                <li>Исполнители откликаются на заявки заказчика.</li>
+                <li>Заказчик оценивает выполненную работу.</li>
               </ul>
+              <hr className="my-2"/>
+              {employee &&
+              employee.role === EmployeeRole.CUSTOMER && (
+                <Link to={"/entity/task-creation"}>Оформить заявку</Link>
+              )}
+              &nbsp;
+              {employee &&
+              employee.role === EmployeeRole.CUSTOMER && (
+                <Link to={"/entity/customer-requests"}>Мои заявки</Link>
+              )}
+              {employee && employee.role === EmployeeRole.PERFORMER && <PerformerHome/>}
             </div>
           ) : (
-            <div style={{ position: 'absolute', top: 20, right: 40 }}>
+            <div style={{ position: "absolute", top: 20, right: 40 }}>
               <Alert color="warning">
                 <h4>Добро пожаловать в "Новую Лигу"!</h4>У вас нет аккаунта? &nbsp;
                 <Link to="/register" className="alert-link">
@@ -84,7 +85,7 @@ const mapDispatchToProps = { getSession, getEmployees };
 type StateProps = ReturnType<typeof mapStateToProps>;
 type DispatchProps = typeof mapDispatchToProps;
 
-export default connect(
+export default connect (
   mapStateToProps,
   mapDispatchToProps
-)(Home);
+) ( Home );
