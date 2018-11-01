62d78d38d16c239bd30f202af97a2b4dd54b070d
Slavent <startupne@gmail.com>
2018-11-01 14:13:39 +0700

    role switch

--- src/main/webapp/app/modules/account/register/register.tsx
+++ src/main/webapp/app/modules/account/register/register.tsx
@@ -12,11 +12,18 @@ export interface IRegisterProps extends StateProps, DispatchProps {}
 
 export interface IRegisterState {
   password: string;
+  role: string;
 }
+
+const ROLES = {
+  PERFORMER: 'PERFORMER',
+  CUSTOMER: 'CUSTOMER'
+};
 
 export class RegisterPage extends React.Component<IRegisterProps, IRegisterState> {
   state: IRegisterState = {
-    password: ''
+    password: '',
+    role: ''
   };
 
   componentWillUnmount() {
@@ -32,7 +39,13 @@ export class RegisterPage extends React.Component<IRegisterProps, IRegisterState
     this.setState({ password: event.target.value });
   };
 
+  onRadioChange = event => {
+    this.setState({ role: event.target.value });
+  };
+
   render() {
+    const { role } = this.state;
+
     return (
       <div>
         <Row className="justify-content-center">
@@ -45,77 +58,71 @@ export class RegisterPage extends React.Component<IRegisterProps, IRegisterState
         <Row className="justify-content-center">
           <Col md="8">
             <AvForm id="register-form" onValidSubmit={this.handleValidSubmit}>
-              <AvRadioGroup name="role" label={translate('global.form.role.title')} required>
-                <AvRadio label={translate('global.form.role.customer')} value="CUSTOMER" />
-                <AvRadio label={translate('global.form.role.performer')} value="PERFORMER" />
+              <AvRadioGroup name="role" label={translate('global.form.role.title')} required onChange={this.onRadioChange}>
+                <AvRadio label={translate('global.form.role.customer')} value={ROLES.CUSTOMER} />
+                <AvRadio label={translate('global.form.role.performer')} value={ROLES.PERFORMER} />
               </AvRadioGroup>
-              <AvField
-                name="username"
-                label={translate('global.form.username')}
-                placeholder={translate('global.form.username.placeholder')}
-                validate={{
-                  required: { value: true, errorMessage: translate('register.messages.validate.login.required') },
-                  pattern: { value: '^[_.@A-Za-z0-9-]*$', errorMessage: translate('register.messages.validate.login.pattern') },
-                  minLength: { value: 1, errorMessage: translate('register.messages.validate.login.minlength') },
-                  maxLength: { value: 50, errorMessage: translate('register.messages.validate.login.maxlength') }
-                }}
-              />
-              <AvField
-                name="email"
-                label={translate('global.form.email')}
-                placeholder={translate('global.form.email.placeholder')}
-                type="email"
-                validate={{
-                  required: { value: true, errorMessage: translate('global.messages.validate.email.required') },
-                  minLength: { value: 5, errorMessage: translate('global.messages.validate.email.minlength') },
-                  maxLength: { value: 254, errorMessage: translate('global.messages.validate.email.maxlength') }
-                }}
-              />
-              <AvField
-                name="firstPassword"
-                label={translate('global.form.newpassword')}
-                placeholder={translate('global.form.newpassword.placeholder')}
-                type="password"
-                onChange={this.updatePassword}
-                validate={{
-                  required: { value: true, errorMessage: translate('global.messages.validate.newpassword.required') },
-                  minLength: { value: 4, errorMessage: translate('global.messages.validate.newpassword.minlength') },
-                  maxLength: { value: 50, errorMessage: translate('global.messages.validate.newpassword.maxlength') }
-                }}
-              />
-              <PasswordStrengthBar password={this.state.password} />
-              <AvField
-                name="secondPassword"
-                label={translate('global.form.confirmpassword')}
-                placeholder={translate('global.form.confirmpassword.placeholder')}
-                type="password"
-                validate={{
-                  required: { value: true, errorMessage: translate('global.messages.validate.confirmpassword.required') },
-                  minLength: { value: 4, errorMessage: translate('global.messages.validate.confirmpassword.minlength') },
-                  maxLength: { value: 50, errorMessage: translate('global.messages.validate.confirmpassword.maxlength') },
-                  match: { value: 'firstPassword', errorMessage: translate('global.messages.error.dontmatch') }
-                }}
-              />
-              <Button id="register-submit" color="primary" type="submit">
-                <Translate contentKey="register.form.button">Register</Translate>
-              </Button>
+
+              {role && (
+                <div>
+                  <AvField
+                    name="username"
+                    label={translate('global.form.username')}
+                    placeholder={translate('global.form.username.placeholder')}
+                    validate={{
+                      required: { value: true, errorMessage: translate('register.messages.validate.login.required') },
+                      pattern: { value: '^[_.@A-Za-z0-9-]*$', errorMessage: translate('register.messages.validate.login.pattern') },
+                      minLength: { value: 1, errorMessage: translate('register.messages.validate.login.minlength') },
+                      maxLength: { value: 50, errorMessage: translate('register.messages.validate.login.maxlength') }
+                    }}
+                  />
+                  <AvField
+                    name="email"
+                    label={translate('global.form.email')}
+                    placeholder={translate('global.form.email.placeholder')}
+                    type="email"
+                    validate={{
+                      required: { value: true, errorMessage: translate('global.messages.validate.email.required') },
+                      minLength: { value: 5, errorMessage: translate('global.messages.validate.email.minlength') },
+                      maxLength: { value: 254, errorMessage: translate('global.messages.validate.email.maxlength') }
+                    }}
+                  />
+                  <AvField
+                    name="firstPassword"
+                    label={translate('global.form.newpassword')}
+                    placeholder={translate('global.form.newpassword.placeholder')}
+                    type="password"
+                    onChange={this.updatePassword}
+                    validate={{
+                      required: { value: true, errorMessage: translate('global.messages.validate.newpassword.required') },
+                      minLength: { value: 4, errorMessage: translate('global.messages.validate.newpassword.minlength') },
+                      maxLength: { value: 50, errorMessage: translate('global.messaupdatePasswordges.validate.newpassword.maxlength') }
+                    }}
+                  />
+                  <PasswordStrengthBar password={this.state.password} />
+                  <AvField
+                    name="secondPassword"
+                    label={translate('global.form.confirmpassword')}
+                    placeholder={translate('global.form.confirmpassword.placeholder')}
+                    type="password"
+                    validate={{
+                      required: { value: true, errorMessage: translate('global.messages.validate.confirmpassword.required') },
+                      minLength: { value: 4, errorMessage: translate('global.messages.validate.confirmpassword.minlength') },
+                      maxLength: { value: 50, errorMessage: translate('global.messages.validate.confirmpassword.maxlength') },
+                      match: { value: 'firstPassword', errorMessage: translate('global.messages.error.dontmatch') }
+                    }}
+                  />
+                  <Button id="register-submit" color="primary" type="submit">
+                    <Translate contentKey="register.form.button">Register</Translate>
+                  </Button>
+                </div>
+              )}
+
+              {role === ROLES.CUSTOMER && <div />}
+
+              {role === ROLES.PERFORMER && <div />}
             </AvForm>
             <p>&nbsp;</p>
-            <Alert color="warning">
-              <span>
-                <Translate contentKey="global.messages.info.authenticated.prefix">If you want to </Translate>
-              </span>
-              <a className="alert-link">
-                <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
-              </a>
-              <span>
-                <Translate contentKey="global.messages.info.authenticated.suffix">
-                  , you can try the default accounts:
-                  <br />- Administrator (login="admin" and password="admin")
-                  <br />- User (login="user" and password="user").
-                </Translate>
-              </span>
-            </Alert>
           </Col>
         </Row>
       </div>
