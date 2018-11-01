2afcabae9b336374a61a4b5865de40649034cede
Slavent <startupne@gmail.com>
2018-11-01 19:56:17 +0700

    check role

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -1,29 +1,34 @@
 import './home.css';
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Translate } from 'react-jhipster';
 import { connect } from 'react-redux';
 import { Row, Col, Alert } from 'reactstrap';
-
-import { IRootState } from 'app/shared/reducers';
 import { getSession } from 'app/shared/reducers/authentication';
-import CustomerHome from 'app/modules/customer-home/customer-home';
+import { getEntities as getEmployees } from 'app/entities/employee/employee.reducer';
 
 export interface IHomeProp extends StateProps, DispatchProps {}
 
 export class Home extends React.Component<IHomeProp> {
   componentDidMount() {
     this.props.getSession();
+    this.props.getEmployees();
   }
 
   render() {
-    const { account } = this.props;
+    const { account, employees, isAuthenticated } = this.props;
+    const employee = employees.find(item => item.username.toLocaleLowerCase() === account.login);
+
     return (
       <Row>
         <Col md="12">
-          {account && account.login ? (
-            <CustomerHome />
+          {isAuthenticated ? (
+            <div>
+              <h2>Здравствуйте, {account.login}!</h2>
+              {employee && employee.role === 'CUSTOMER' && <Link to={'/entity/task-creation'}>Оформить заявку</Link>}
+
+              {employee && employee.role === 'PERFORMER' && <Link to={'/entity/tasks-performer'}>Просмотреть мои заявки</Link>}
+            </div>
           ) : (
             <div>
               <Alert color="warning">
@@ -49,18 +54,20 @@ export class Home extends React.Component<IHomeProp> {
             </div>
           )}
         </Col>
-        <Col md="3" className="pad" />
       </Row>
     );
   }
 }
 
-const mapStateToProps = storeState => ({
-  account: storeState.authentication.account,
-  isAuthenticated: storeState.authentication.isAuthenticated
-});
+const mapStateToProps = storeState => {
+  return {
+    account: storeState.authentication.account,
+    isAuthenticated: storeState.authentication.isAuthenticated,
+    employees: storeState.employee.entities
+  };
+};
 
-const mapDispatchToProps = { getSession };
+const mapDispatchToProps = { getSession, getEmployees };
 
 type StateProps = ReturnType<typeof mapStateToProps>;
 type DispatchProps = typeof mapDispatchToProps;
